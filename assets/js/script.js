52955bcb222892cb39dcfeae90416f2dd1372a5e
Md. Razwanul Hasan <razwanulhasan@gmail.com>
2025-01-30 10:40:08 +0600

    just change the cv

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,18 @@
-'use strict';
-
-
+"use strict";
 
 // element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
 
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
@@ -31,29 +29,27 @@ const modalText = document.querySelector("[data-modal-text]");
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
-}
+};
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
-
   testimonialsItem[i].addEventListener("click", function () {
-
     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalTitle.innerHTML = this.querySelector(
+      "[data-testimonials-title]"
+    ).innerHTML;
+    modalText.innerHTML = this.querySelector(
+      "[data-testimonials-text]"
+    ).innerHTML;
 
     testimonialsModalFunc();
-
   });
-
 }
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
-
 
 // custom select variables
 const select = document.querySelector("[data-select]");
@@ -61,17 +57,17 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-select-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+select.addEventListener("click", function () {
+  elementToggleFunc(this);
+});
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     elementToggleFunc(select);
     filterFunc(selectedValue);
-
   });
 }
 
@@ -79,9 +75,7 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
   for (let i = 0; i < filterItems.length; i++) {
-
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
@@ -89,18 +83,14 @@ const filterFunc = function (selectedValue) {
     } else {
       filterItems[i].classList.remove("active");
     }
-
   }
-
-}
+};
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-
   filterBtn[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     filterFunc(selectedValue);
@@ -108,12 +98,8 @@ for (let i = 0; i < filterBtn.length; i++) {
     lastClickedBtn.classList.remove("active");
     this.classList.add("active");
     lastClickedBtn = this;
-
   });
-
 }
-
-
 
 // contact form variables
 const form = document.querySelector("[data-form]");
@@ -140,13 +126,13 @@ form.addEventListener("input", function () {
     formBtn.setAttribute("disabled", "");
   }
 });
-  formBtn.addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-    // Show a message to the user
-    alert("This process is under development. Please contact whatsapp or LinkedIn");
-  });
-
-
+formBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+  // Show a message to the user
+  alert(
+    "This process is under development. Please contact whatsapp or LinkedIn"
+  );
+});
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
@@ -155,7 +141,6 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
@@ -166,7 +151,6 @@ for (let i = 0; i < navigationLinks.length; i++) {
         navigationLinks[i].classList.remove("active");
       }
     }
-
   });
 }
 
@@ -184,21 +168,21 @@ function openModal(imageSrc, bodyContent) {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
 // Add event listeners to each project item
 const projectItems = document.querySelectorAll(".project-item");
-projectItems.forEach(item => {
-  item.addEventListener("click", function() {
+projectItems.forEach((item) => {
+  item.addEventListener("click", function () {
     const imageSrc = this.querySelector("img").src;
     const bodyContent = this.querySelector(".project-category").textContent;
     openModal(imageSrc, bodyContent);
@@ -206,25 +190,27 @@ projectItems.forEach(item => {
 });
 
 // Function to handle download button click
-document.getElementById("downloadPdfBtn").addEventListener("click", function() {
-  // Replace 'path_to_your_pdf_file.pdf' with the actual path to your PDF file
-  const pdfUrl = '/Razwanul_Hasan_Professional_CV5.pdf';
-  
-  // Create an anchor element
-  const anchorElement = document.createElement('a');
-  
-  // Set the href attribute to the PDF URL
-  anchorElement.href = pdfUrl;
-  
-  // Set the download attribute to force download
-  anchorElement.download = '/Razwanul_Hasan_Professional_CV5.pdf';
-  
-  // Append the anchor element to the body
-  document.body.appendChild(anchorElement);
-  
-  // Trigger a click event on the anchor element
-  anchorElement.click();
-  
-  // Remove the anchor element from the body
-  document.body.removeChild(anchorElement);
-});
+document
+  .getElementById("downloadPdfBtn")
+  .addEventListener("click", function () {
+    // Replace 'path_to_your_pdf_file.pdf' with the actual path to your PDF file
+    const pdfUrl = "/Razwanul_CV.pdf";
+
+    // Create an anchor element
+    const anchorElement = document.createElement("a");
+
+    // Set the href attribute to the PDF URL
+    anchorElement.href = pdfUrl;
+
+    // Set the download attribute to force download
+    anchorElement.download = "/Razwanul_CV.pdf";
+
+    // Append the anchor element to the body
+    document.body.appendChild(anchorElement);
+
+    // Trigger a click event on the anchor element
+    anchorElement.click();
+
+    // Remove the anchor element from the body
+    document.body.removeChild(anchorElement);
+  });
