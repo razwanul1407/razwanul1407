11b3135967ae2ee0b4fa1ae174ca406b3cf830be
Md. Razwanul Hasan <razwanulhasan@gmail.com>
2026-02-18 15:07:08 +0600

    Add all projects with Play Store & App Store links and icons
    - Added 9 published apps with Play Store CDN icons as portfolio images
    - New projects: BIFLE User, BIFLE Driver, Tamworth '24, BRAC Fleet,
      Realty Pro Shots, Beautysiaa, Hillive, Quality Foods, QIL Sales
    - Play Store + App Store buttons rendered in project modal with SVG icons
    - Added .playstore (green) and .appstore (dark) button styles in CSS
    - Square Play Store icons use object-fit: contain with dark background
    - Retained 7 existing projects (Pujam Pandal, Vip Satta, MyC2i, etc.)

--- assets/js/script.js
+++ assets/js/script.js
@@ -433,9 +433,23 @@ function bindProjectModal() {
           });
         }
 
-        // Build links (GitHub + Live)
+        // Build links (GitHub + Live + Play Store + App Store)
         if (modalLinksEl) {
           modalLinksEl.innerHTML = "";
+          if (project.playstoreUrl) {
+            modalLinksEl.innerHTML += `
+              <a href="${project.playstoreUrl}" class="project-modal-link playstore" target="_blank" rel="noopener">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M3.18 23.76c.31.17.67.19 1 .08l11.62-6.71L13.1 14.4l-9.92 9.36zm-1.12-21C2.03 3.1 2 3.44 2 3.8v16.4c0 .36.03.7.06 1.04L13.5 12 2.06 2.76zM20.1 9.27l-2.42-1.4-3.03 2.85 3.03 2.85 2.44-1.41c.7-.4.7-1.49-.02-1.89zM4.18.16L15.8 6.87 13.1 9.6 1.48.24C1.8.1 2.15.1 2.47.24l1.71-.08z"/></svg>
+                Play Store
+              </a>`;
+          }
+          if (project.appstoreUrl) {
+            modalLinksEl.innerHTML += `
+              <a href="${project.appstoreUrl}" class="project-modal-link appstore" target="_blank" rel="noopener">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.8-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/></svg>
+                App Store
+              </a>`;
+          }
           if (project.githubUrl) {
             modalLinksEl.innerHTML += `
               <a href="${project.githubUrl}" class="project-modal-link github" target="_blank" rel="noopener">
